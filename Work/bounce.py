0c21ca2623e217b407e467e5a48fb96be2bee14a
Skux4life <mattellis1234@gmail.com>
2023-10-11 18:31:26 +1100

    bounce and sears

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,13 @@
 # bounce.py
 #
 # Exercise 1.5
+
+def main():
+    height = 100
+    bouncesLeft = 1
+    while bouncesLeft <= 10:
+        height = height * 3 / 5
+        print(bouncesLeft, round(height, 4))
+        bouncesLeft += 1
+
+main()
