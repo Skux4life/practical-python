cb1407aa24dc0e1cd8c68ca9d65e299c959a4d98
Skux4life <mattellis1234@gmail.com>
2023-10-14 19:37:38 +1100

    pcost and report

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,27 @@
 # pcost.py
 #
 # Exercise 1.27
+import sys
+
+def portfolio_cost(filename):
+    total = 0
+    with open(filename, 'rt') as f:
+        headers = next(f)
+        for line in f:
+            row = line.split(',')
+            try:
+                total = total + (int(row[1]) * float(row[2]))
+            except ValueError:
+                print("Couldn't parse line", line)
+    return total
+
+
+def main():
+    if len(sys.argv) == 2:
+        filename = sys.argv[1]
+    else:
+        filename = 'Data/portfolio.csv'
+    cost = portfolio_cost(filename)
+    print('Total cost', cost)
+
+main()
--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,37 @@
 # report.py
 #
 # Exercise 2.4
+import csv
+from pprint import pprint
+
+def read_portfolio(filename):
+    '''Reads the portfolio from a CSV file'''
+    portfolio = []
+    with open(filename, 'rt') as f:
+        reader = csv.reader(f)
+        header = next(reader)
+        for row in reader:
+            holding = (row[0], int(row[1]), float(row[2]))
+            portfolio.append(holding)
+    return portfolio
+
+def read_portfolio_dict(filename):
+    '''Reads the portfolio from a CSV file'''
+    portfolio = []
+    with open(filename, 'rt') as f:
+        reader = csv.reader(f)
+        header = next(reader)
+        for row in reader:
+            holding = {
+                'name': row[0],
+                'shares': int(row[1]),
+                'price': float(row[2])
+            }
+            portfolio.append(holding)
+    return portfolio
+
+def main():
+    portfolio = read_portfolio_dict('Data/portfolio.csv')
+    pprint(portfolio)
+
+main()
